4a070873e633cec76cf5b759f8a9ccf1fc23a708
onebit0fme <yanchynskyy@pm.me>
2022-10-05 20:11:42 -0400

    Basic stats for RunForUkraine

---
+++ clients.py
@@ -0,0 +1,114 @@
+import os
+import streamlit as st
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+
+WAVE_TOKEN = st.secrets['wave']['WAVE_TOKEN']
+WAVE_URL = "https://gql.waveapps.com/graphql/public"
+WAVE_BUSINESS_ID = st.secrets['wave']["WAVE_BUSINESS_ID"]
+
+
+INVOICES_QUERY = gql("""query($businessId: ID!, $page: Int!, $slug: String!) {
+  business(id: $businessId) {
+    id
+    invoices(
+      page: $page,
+      invoiceNumber: $slug
+    ) {
+      edges {
+        node {
+          id
+          title
+          subhead
+          invoiceNumber
+          footer
+          memo
+          status
+          amountDue {
+            raw
+            value
+          }
+          amountPaid {
+            raw
+            value
+          }
+          total {
+            raw
+            value
+          }
+          customer {
+            id
+            name
+            email
+            shippingDetails {
+                name
+                phone
+                address {
+                    addressLine1
+                    addressLine2
+                    city
+                    province {
+                        code
+                        name
+                    }
+                    country {
+                        code
+                        name
+                    }
+                    postalCode
+                }
+            }
+          }
+          items {
+            description
+            quantity
+            unitPrice
+            product {
+              id
+              name
+            }
+          }
+        }
+      }
+      pageInfo {
+        totalPages
+        currentPage
+        totalCount
+      }
+    }
+  }
+}""")
+
+class WaveClient:
+    """
+    Client for Wave GraphQL API.
+
+    API Playgound - https://developer.waveapps.com/hc/en-us/articles/360018937431-API-Playground
+    """
+
+    def __init__(self, business_id=WAVE_BUSINESS_ID):
+        self.business_id = business_id
+        headers = {"Authorization": f"Bearer {WAVE_TOKEN}"}
+        transport = AIOHTTPTransport(url=WAVE_URL, headers=headers)
+        self.client = Client(transport=transport)
+    
+    def get_invoices_for_slug(self, slug: str):
+        invoices = []
+        page = 1
+        while True:
+            response = self.client.execute(INVOICES_QUERY, variable_values={
+                'businessId': self.business_id,
+                'page': page,
+                'slug': slug.upper(),
+            })
+            invoices.extend(response['business']['invoices']['edges'])
+
+            # Check for next page
+            total_pages = response['business']['invoices']['pageInfo']['totalPages']
+            if total_pages >= page:
+                break
+            page += 1
+        
+        return invoices
+
---
+++ main.py
@@ -0,0 +1,106 @@
+from tkinter import W
+import streamlit as st
+import pandas as pd
+from clients import WaveClient
+
+wave = WaveClient()
+
+
+@st.experimental_memo(ttl=3600)
+def get_runforukraine_invoices():
+    return wave.get_invoices_for_slug("2FUA-RUN4UA")
+
+
+def invoices_to_df(invoices):
+    data = []
+
+    for inv in invoices:
+        shipping_details = inv['node']['customer'].get('shippingDetails') or {}
+        shipping_address = shipping_details.get('address') or {}
+
+        data.append({
+            'memo': inv['node']['memo'],
+            'status': inv['node']['status'],
+            'amountDue': inv['node']['amountDue']['value'],
+            'amountPaid': inv['node']['amountPaid']['value'],
+            'total': inv['node']['total']['value'],
+            'customer_name': inv['node']['customer']['name'],
+            'customer_email': inv['node']['customer']['email'],
+            'customer_phone': shipping_details.get('phone'),
+            'address_line_1': shipping_address.get('addressLine1'),
+            'address_line_2': shipping_address.get('addressLine2'),
+            'address_city': shipping_address.get('city'),
+            'address_province': (shipping_address.get('province') or {}).get('name'),
+            'address_country': (shipping_address.get('country') or {}).get('name'),
+            'address_postal_code': shipping_address.get('postalCode', None),
+        })
+
+    df = pd.DataFrame(data)
+    df['total'] = df['total'].astype(float)
+    df['amountDue'] = df['amountDue'].astype(float)
+    df['amountPaid'] = df['amountPaid'].astype(float)
+
+    return df
+
+
+def invoices_to_items_df(invoices):
+    data = []
+
+    for inv in invoices:
+        shipping_details = inv['node']['customer'].get('shippingDetails') or {}
+        shipping_address = shipping_details.get('address') or {}
+        for item in inv['node']['items']:
+            data.append({
+                'memo': inv['node']['memo'],
+                'status': inv['node']['status'],
+                'amountDue': inv['node']['amountDue']['value'],
+                'amountPaid': inv['node']['amountPaid']['value'],
+                'total': inv['node']['total']['value'],
+                'customer_name': inv['node']['customer']['name'],
+                'customer_email': inv['node']['customer']['email'],
+                'customer_phone': shipping_details.get('phone'),
+                'address_line_1': shipping_address.get('addressLine1'),
+                'address_line_2': shipping_address.get('addressLine2'),
+                'address_city': shipping_address.get('city'),
+                'address_province': (shipping_address.get('province') or {}).get('name'),
+                'address_country': (shipping_address.get('country') or {}).get('name'),
+                'address_postal_code': shipping_address.get('postalCode', None),
+                'quantity': item['quantity'],
+                'description': item['description'],
+                'unitPrice': item['unitPrice'],
+                'id': item['product']['id'],
+                'name': item['product']['name'],
+            })
+
+    df = pd.DataFrame(data)
+    df['total'] = df['total'].astype(float)
+    df['amountDue'] = df['amountDue'].astype(float)
+    df['amountPaid'] = df['amountPaid'].astype(float)
+    df['unitPrice'] = df['unitPrice'].astype(float)
+    df['quantity'] = df['quantity'].astype(int)
+
+    return df
+
+invoices = get_runforukraine_invoices()
+# st.write(invoices)
+df = invoices_to_df(invoices)
+items_df = invoices_to_items_df(invoices)
+
+df_paid = df[df['status'] == 'PAID']
+invoices_df_unpaid = df[(df['status'] != 'PAID') & (df['status'] != 'DRAFT')]
+items_df_paid = items_df[items_df['status'] == 'PAID']
+
+c0, c1, c2 = st.columns(3)
+c0.metric("Total collected", f"{df_paid['amountPaid'].sum():.2f}")
+c1.metric("Total registered", len(df_paid))
+c2.metric("Total abandoned", len(invoices_df_unpaid))
+st.write("By country")
+st.table(df_paid.groupby('address_country').count()['customer_name'])
+
+st.write("By item")
+st.table(items_df_paid.groupby('name').count()['customer_name'])
+
+with st.expander("All invoices"):
+    st.write(df)
+with st.expander("All items"):
+    st.write(items_df)
